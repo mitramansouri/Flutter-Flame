431bf51bf22a23f2c3a68f62995e691a53a78236
Mitr <MITRAMANSOURI78@GMAIL.COM>
2022-05-18 17:41:37 +0430

    player works

--- tutorial/lib/components/player.dart
+++ tutorial/lib/components/player.dart
@@ -1,10 +1,14 @@
 import 'package:flame/components.dart';
-import 'package:flutter/foundation.dart';
+
+import '../helpers/direction.dart';
 
 class Player extends SpriteComponent with HasGameRef {
+  Direction direction = Direction.none;
+  final double _playerSpeed = 300.0;
+
   Player()
   :super(
-    size: Vector2.all(50.0),
+    size: Vector2(150.0,150.0),
   );
 
   @override 
@@ -13,4 +17,45 @@ class Player extends SpriteComponent with HasGameRef {
     sprite = await gameRef.loadSprite('player.png');
     position = gameRef.size / 2;
   }
+
+  @override 
+  void update(double dt){
+    super.update(dt);
+    movePlayer(dt);
+  }
+  void movePlayer(delta){
+    switch (direction) {
+      case Direction.up:
+        moveUp(delta);
+        break;
+      case Direction.down:
+        moveDown(delta);
+        break;
+      case Direction.left:
+        moveLeft(delta);
+        break;
+      case Direction.right:
+        moveRight(delta);
+        break;
+      case Direction.none:
+        break;
+    }
+
+  }
+void moveDown(double delta) {
+   position.add(Vector2(0, delta * _playerSpeed));
+}
+
+void moveUp(double delta) {
+  position.add(Vector2(0, delta * -_playerSpeed));
+}
+ 
+void moveLeft(double delta) {
+  position.add(Vector2(delta * -_playerSpeed, 0));
+}
+ 
+void moveRight(double delta) {
+  position.add(Vector2(delta * _playerSpeed, 0));
+}
+
 }
--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -3,24 +3,46 @@ import 'dart:html';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tutorial/components/player.dart';
 
+import 'package:flame/input.dart';
+import 'package:tutorial/helpers/direction.dart';
 void main() {
   runApp(GameWidget(game: MyGame()));
 }
 
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with KeyboardEvents {
   final Player _player = Player();
   @override 
   Future<void> onLoad() async{
     add(_player);
   }
 
-  @override 
-  void update(double dt) {
-    // TODO: implement update
-    super.update(dt);
-    _player.y +1 ;
+  @override
+  KeyEventResult onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isKeyDown = event is RawKeyDownEvent;
+    Direction? keyDirection = null;
+
+    if (event.logicalKey == LogicalKeyboardKey.keyA) {
+      keyDirection = Direction.left;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyD) {
+      keyDirection = Direction.right;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyW) {
+      keyDirection = Direction.up;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyS) {
+      keyDirection = Direction.down;
+    }
+
+
+    if (isKeyDown && keyDirection != null) {
+      _player.direction = keyDirection;
+    } else if (_player.direction == keyDirection) {
+      _player.direction = Direction.none;
+    }
+    return super.onKeyEvent(event, keysPressed);
   }
+
+
 }
